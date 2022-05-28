41b3766c52a6c70f431dece3561584b74a22d32f
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-05-28 14:51:26 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome Paint Brush')
+})
+
+app.listen(port, () => {
+    console.log(`Paint brush in on ${port}`);
+})
