df042b247c839f795b37b35ad6ab4d337fb1acec
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-05-29 16:01:12 +0600

    admin api created

--- index.js
+++ index.js
@@ -74,6 +74,12 @@ async function run() {
             const result = await orderCollection.insertOne(order);
             return res.send({ success: true, result });
         })
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email })
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+        })
 
     }
     finally {
