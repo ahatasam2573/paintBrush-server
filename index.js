20bfe5c27d8918ef005cad3c02f318c939ea617c
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-05-29 16:14:00 +0600

    admin role api created

--- index.js
+++ index.js
@@ -80,6 +80,22 @@ async function run() {
             const isAdmin = user.role === 'admin';
             res.send({ admin: isAdmin });
         })
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+        })
 
     }
     finally {
